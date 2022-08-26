9d22df53f6760cec3cfa14135456731a1d462129
Tyler Savery <tyler@theyoungastronauts.com>
2022-08-26 08:57:52 -0400

    service domain update

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://data.rbx.network/api";
+export const API_BASE_URL = "https://rbx-explorer-service.herokuapp.com/api";
 // export const API_BASE_URL = "http://127.0.0.1:8000/api";
 export const MAINTENENCE_MODE = false;
