5a1fc170718510e1cf605fc20cb96b87bde94fee
Tyler Savery <tyler@theyoungastronauts.com>
2023-02-22 13:31:09 -0500

    mobile for new validators

--- pages/validators/new/index.tsx
+++ pages/validators/new/index.tsx
@@ -1,13 +1,13 @@
 import type { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { isMobile } from "react-device-detect";
+import { ValidatorCardList } from "../../../src/components/validator-card-list";
 import { ValidatorList } from "../../../src/components/validator-list";
 import { API_BASE_URL, IS_TESTNET } from "../../../src/constants";
 import { Validator } from "../../../src/models/validator";
 
-interface Props {
-  validators: Validator[];
-}
+
 
 const ValidatorPoolPage: NextPage = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   if (typeof window === "undefined") {
@@ -16,36 +16,41 @@ const ValidatorPoolPage: NextPage = ({ data }: InferGetServerSidePropsType<typeo
   const results: any[] = data.results;
   const validators: Validator[] = results.map(v => new Validator(v));
 
-  console.log(validators);
-
-
   return (
-    <div>
+    <>
       <Head>
         <title>RBX Explorer: Validator Pool{IS_TESTNET ? ' [TESTNET]' : ''}</title>
         <meta name="description" content="ReserveBlock Explorer: Home" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-
-
-      <div className="bg-dark text-end px-3 py-2">
-        <div className="d-flex justify-content-end align-items-center">
-
+      {isMobile ?
+        <div className="text-center px-3 pt-2">
           {validators.length ? (
             <div className="d-inline-block bg-success h6 rounded py-1 px-2 mb-0">
               Total Active Validators: {validators.filter(v => v.isActive).length}
             </div>) : null}
-          <div>
-            <Link href={"/validators/search"}>
-              <a className="btn-link btn btn-sm" >Check Validator Status</a>
-            </Link>
-          </div>
+          <Link href={"/validators/search"}>
+            <a className="btn-link btn btn-sm" >Check Validator Status</a>
+          </Link>
         </div>
-      </div>
+        :
+        <div className="bg-dark text-end px-3 py-2">
+          <div className="d-flex justify-content-end align-items-center">
 
-      <ValidatorList validators={validators} />
+            {validators.length ? (
+              <div className="d-inline-block bg-success h6 rounded py-1 px-2 mb-0">
+                Total Active Validators: {validators.filter(v => v.isActive).length}
+              </div>) : null}
+            <div>
+              <Link href={"/validators/search"}>
+                <a className="btn-link btn btn-sm" >Check Validator Status</a>
+              </Link>
+            </div>
+          </div>
+        </div>}
 
-    </div>
+      {isMobile ? <ValidatorCardList validators={validators} /> : <ValidatorList validators={validators} />}
+    </>
   );
 };
 
