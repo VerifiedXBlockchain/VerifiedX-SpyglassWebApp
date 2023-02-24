d753636ca330cf524f4fb2b6651af55d74fe2501
Tyler Savery <tyler@theyoungastronauts.com>
2023-02-24 16:07:14 -0500

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,13 @@ import { BlockListContainer } from "../src/components/block-list-container";
 import { API_BASE_URL, IS_TESTNET } from "../src/constants";
 import { Block } from "../src/models/block";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   if (typeof window === "undefined") {
     return null;
   }
 
-
+  const results: any[] = data.results;
+  const blocks: Block[] = results.map(b => new Block(b));
 
   return (
     <div>
@@ -22,9 +23,29 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      {isMobile ? <BlockListContainer initialBlocks={[]} /> : <BlockRowsContainer initialBlocks={[]} />}
+      {isMobile ? <BlockListContainer initialBlocks={blocks} /> : <BlockRowsContainer initialBlocks={blocks} />}
     </div>
   );
 };
 
 export default Home;
+
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=30, stale-while-revalidate=59'
+  )
+
+  const url = `${API_BASE_URL}/blocks/?page=1`;
+  const result = await fetch(url)
+  const data: any[] = await result.json()
+
+
+  return {
+    props: {
+      data: data
+    },
+  }
+}
--- pages/old.tsx
+++ pages/old.tsx
@@ -7,13 +7,12 @@ import { BlockListContainer } from "../src/components/block-list-container";
 import { API_BASE_URL, IS_TESTNET } from "../src/constants";
 import { Block } from "../src/models/block";
 
-const Home: NextPage = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const Home: NextPage = () => {
     if (typeof window === "undefined") {
         return null;
     }
 
-    const results: any[] = data.results;
-    const blocks: Block[] = results.map(b => new Block(b));
+
 
     return (
         <div>
@@ -23,29 +22,9 @@ const Home: NextPage = ({ data }: InferGetServerSidePropsType<typeof getServerSi
                 <link rel="icon" href="/favicon.png" />
             </Head>
 
-            {isMobile ? <BlockListContainer initialBlocks={blocks} /> : <BlockRowsContainer initialBlocks={blocks} />}
+            {isMobile ? <BlockListContainer initialBlocks={[]} /> : <BlockRowsContainer initialBlocks={[]} />}
         </div>
     );
 };
 
 export default Home;
-
-
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-
-    res.setHeader(
-        'Cache-Control',
-        'public, s-maxage=30, stale-while-revalidate=59'
-    )
-
-    const url = `${API_BASE_URL}/blocks/?page=1`;
-    const result = await fetch(url)
-    const data: any[] = await result.json()
-
-
-    return {
-        props: {
-            data: data
-        },
-    }
-}
