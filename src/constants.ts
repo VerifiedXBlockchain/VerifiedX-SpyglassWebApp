a0813d9a6f191664eed6e4732c99a22517247796
Tyler Savery <tyler@theyoungastronauts.com>
2024-05-13 21:49:02 -0400

    oops constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
-// export const IS_TESTNET = process.env.NEXT_PUBLIC_IS_TESTNET == "true" ? true : false;
-// export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://data.rbx.network/api";
-export const API_BASE_URL = "http://127.0.0.1:8000/api";
-export const IS_TESTNET = true;
+export const IS_TESTNET = process.env.NEXT_PUBLIC_IS_TESTNET == "true" ? true : false;
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://data.rbx.network/api";
+// export const API_BASE_URL = "http://127.0.0.1:8000/api";
+// export const IS_TESTNET = true;
 
 
 
